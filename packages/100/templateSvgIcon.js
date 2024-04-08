0192159a8e24ff4adb24b70741f806702a0a0a20
BEAR <RubtsovAV@gmail.com>
2024-04-08 12:32:01 +0000

    createSvgIcon with defaultProps because React.defaultProps has been depricated

--- packages/100/templateSvgIcon.js
+++ packages/100/templateSvgIcon.js
@@ -2,10 +2,11 @@
 import createSvgIcon from './utils/createSvgIcon';
 
 const SvgIcon = createSvgIcon(
-  {{{paths}}}
-, '{{componentName}}');
-SvgIcon.defaultProps = {
+  {{{ paths }}}, 
+  '{{componentName}}', 
+  {
   viewBox: "0 -960 960 960",
-};
+  }
+);
 export default SvgIcon;
 
---
+++ packages/200/src/utils/createSvgIcon.tsx
@@ -0,0 +1,28 @@
+'use client';
+import * as React from 'react';
+import SvgIcon, {SvgIconProps} from '@mui/material';
+
+/**
+ * @source https://github.com/mui/material-ui/packages/mui-material/src/utils/createSvgIcon.js
+ */
+export default function createSvgIcon(path: React.ReactNode, displayName: string, defaultProps: Partial<SvgIconProps> = {}) {
+  function Component(props: SvgIconProps, ref: React.ForwardedRef<typeof SvgIcon>) {
+    return (
+      <SvgIcon data-testid={`${displayName}Icon`} ref={ref} {...defaultProps} {...props}>
+        {path}
+      </SvgIcon>
+    );
+  }
+
+  //@ts-ignore
+  if (process.env.NODE_ENV !== 'production') {
+    // Need to set `displayName` on the inner component for React.memo.
+    // React prior to 16.14 ignores `displayName` on the wrapper.
+    Component.displayName = `${displayName}Icon`;
+  }
+
+  //@ts-ignore
+  Component.muiName = SvgIcon.muiName;
+
+  return React.memo(React.forwardRef(Component));
+}
